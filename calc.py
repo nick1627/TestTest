5d4997e81e2e2b48c95cc0cf6a932100cbed7994
nick1627 <nick.rayns1627@gmail.com>
2023-03-12 20:19:36 +0000

    added some dumb maths functions

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    """Addition function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication function"""
+    return x*y
+
+def divide(x, y):
+    """Division function"""
+    return x/y
